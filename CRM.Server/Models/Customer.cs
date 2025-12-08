24997bf35ffd6218d814357b703440f448bc9d95
EswarSaiVeera <eswarsaiveera@gmail.com>
2025-12-08 15:57:52 +0530

    added customer model

--- CRM.Server/Models/Customer.cs
+++ CRM.Server/Models/Customer.cs
@@ -3,5 +3,12 @@
     public class Customer
     {
         public Guid CustomerId {  get; set; }
+
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string CreatedByUserId { get; set; }
+        public string CreatedAt { get; set; }
     }
 }
